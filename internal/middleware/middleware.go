92d6ed7e49db571d769eb3ab94f9e067ac6854cb
TLOWAC <leechanghun55@gmail.com>
2024-11-03 03:50:50 +0900

    feat(middleware): requesstid, recover 미들웨어 추가

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -2,15 +2,21 @@ package middleware
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
 )
 
 func SetupMiddleware(app *fiber.App) *fiber.App {
 
 	// http logging middleware
+	app.Use(requestid.New())
 	app.Use(HttpLoggingMiddleware())
 
 	// cors middleware
 	app.Use(CorsMiddleware())
+
+	// panic recovery middleware
+	app.Use(recover.New())
 
 	return app
 }
