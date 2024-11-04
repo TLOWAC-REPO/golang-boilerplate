76320306bf57498f087983b2b12bba3c286bd1e1
TLOWAC <leechanghun55@gmail.com>
2024-11-04 21:22:46 +0900

    feat: route, middleware, shutdown, setSchema 구성

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	R "golang-boilerplate/api/routes"
+	"golang-boilerplate/ent/migrate"
+	"golang-boilerplate/internal/db"
+	M "golang-boilerplate/internal/middleware"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/gofiber/fiber/v2"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	run()
+}
+
+func run() {
+
+	app := fiber.New()
+	M.SetupMiddleware(app)
+
+	// DB Schema 마이그레이션
+	setSchema()
+
+	R.SetupRoutes(app)
+
+	// Listen from a different goroutine
+	go func() {
+		if err := app.Listen(":3000"); err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)                    // Create channel to signify a signal being sent
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM) // When an interrupt or termination signal is sent, notify the channel
+
+	_ = <-c // This blocks the main thread until an interrupt is received
+	fmt.Println("Gracefully shutting down...")
+	_ = app.Shutdown()
+
+	fmt.Println("Running cleanup tasks...")
+
+	// Your cleanup tasks go here
+	db.Client.Close()
+	// redisConn.Close()
+	fmt.Println("Fiber was successful shutdown.")
+}
+
+func setSchema() {
+	// Run the auto migration tool.
+	if err := db.Client.Schema.Create(context.Background(), migrate.WithForeignKeys(true)); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+}
