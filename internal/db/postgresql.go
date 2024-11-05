544e57ce5faf2c07ebdf537e8cc87d7cb17f56cb
TLOWAC <leechanghun55@gmail.com>
2024-11-05 18:00:55 +0900

    refactor(db): viper 를 사용한 dsn 생성 방식 적용

--- internal/db/postgresql.go
+++ internal/db/postgresql.go
@@ -1,9 +1,13 @@
 package db
 
 import (
+	"fmt"
 	"golang-boilerplate/ent"
+	"golang-boilerplate/internal/config"
 	"log"
 	"sync"
+
+	"entgo.io/ent/dialect"
 )
 
 var (
@@ -14,9 +18,17 @@ var (
 // GetClient는 데이터베이스 클라이언트를 반환합니다.
 // 클라이언트가 아직 초기화되지 않았다면, 초기화합니다.
 func GetClient() *ent.Client {
+	dsn := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable",
+		config.Env.DBUser,
+		config.Env.DBPwd,
+		config.Env.DBHost,
+		config.Env.DBPort,
+		config.Env.DBName,
+	)
+
 	once.Do(func() {
 		var err error
-		Client, err = ent.Open("postgres", "host=localhost port=5432 user=postgres dbname=postgres password=postgres123 sslmode=disable")
+		Client, err = ent.Open(dialect.Postgres, dsn)
 		if err != nil {
 			log.Fatalf("failed opening connection to postgres: %v", err)
 		}
